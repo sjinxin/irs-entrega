06392317bc922d92a5bbf846beb01a6422edbb8f
sjinxin <jinxin@kth.se>
2024-06-30 15:50:59 +0100

    export to pre-filled xml file
    Signed-off-by: sjinxin <jinxin@kth.se>

--- src/irs.py
+++ src/irs.py
@@ -1,15 +1,18 @@
 import typing as t
 from datetime import datetime
+import xml.etree.ElementTree as ET
 
-# from degiro.import Portfolio
 import attrs
 import attr
 import logging
+import pathlib
 from tabulate import tabulate
 
 logging.basicConfig(level=logging.DEBUG)
 
 _logger = logging.getLogger(__name__)
+
+XML_NS = "{http://www.dgci.gov.pt/2009/Modelo3IRSv2024}"
 
 
 @attrs.define
@@ -52,10 +55,43 @@ class CapitalGains:
 
     sales_of_shares_and_securities: t.List[SaleRecord] = attr.field(factory=list)
 
-    def declare(self, sales):
-        for sale in sales:
+    def generate_content(self, parent, ns, line: SaleRecord):
+        linha = ET.SubElement(parent, f"{ns}AnexoJq092AT01-Linha")
+        linha.set("numero", str(line.linha))
+        ET.SubElement(linha, f"{ns}NLinha").text = str(line.linha)
+        ET.SubElement(linha, f"{ns}CodPais").text = str(line.coutry_of_origin)
+        ET.SubElement(linha, f"{ns}Codigo").text = str(line.code)
+        ET.SubElement(linha, f"{ns}AnoRealizacao").text = str(
+            line.realization_date.year
+        )
+        ET.SubElement(linha, f"{ns}MesRealizacao").text = str(
+            line.realization_date.month
+        )
+        ET.SubElement(linha, f"{ns}DiaRealizacao").text = str(line.realization_date.day)
+        ET.SubElement(linha, f"{ns}ValorRealizacao").text = str(
+            round(line.realization_value, 2)
+        )
+        ET.SubElement(linha, f"{ns}AnoAquisicao").text = str(line.acquisition_date.year)
+        ET.SubElement(linha, f"{ns}MesAquisicao").text = str(
+            line.acquisition_date.month
+        )
+        ET.SubElement(linha, f"{ns}DiaAquisicao").text = str(line.acquisition_date.day)
+        ET.SubElement(linha, f"{ns}ValorAquisicao").text = str(
+            round(line.acquisition_value, 2)
+        )
+        ET.SubElement(linha, f"{ns}DespesasEncargos").text = str(
+            round(line.expenses, 2)
+        )
+        ET.SubElement(linha, f"{ns}CodPaisContraparte").text = str(
+            line.coutry_of_origin
+        )
+
+    def declare(self, sales, xml_root):
+        _logger.info(xml_root)
+        for index, sale in enumerate(sales):
             line = SaleRecord(**sale)
-            _logger.debug(line)
+            line.linha += index
+            self.generate_content(xml_root, XML_NS, line)
 
 
 @attrs.define
@@ -64,8 +100,10 @@ class Quadro9:
 
     cap_gains: CapitalGains = attr.field(factory=CapitalGains)
 
-    def declare(self, sales):
-        self.cap_gains.declare(sales)
+    def declare(self, sales, xml_root):
+        self.cap_gains.declare(
+            sales, xml_root=xml_root.find(f".//{XML_NS}AnexoJq092AT01")
+        )
 
 
 @attrs.define
@@ -74,13 +112,30 @@ class AnexoJ:
 
     quadro9: Quadro9 = attr.field(factory=Quadro9)
 
-    def declare(self, sales):
-        self.quadro9.declare(sales)
+    def declare(self, sales, xml_root):
+        self.quadro9.declare(sales, xml_root)
 
 
 @attrs.define
 class IRS:
     annex_j: AnexoJ = attr.field(factory=AnexoJ)
+    root: ET.Element = attr.field(default=None)
 
     def declare(self, sales, fiscal_year):
-        self.annex_j.declare(sales)
+        self.annex_j.declare(sales, xml_root=self.root.find(f".//{XML_NS}AnexoJ"))
+
+    def load(self, file: pathlib.Path):
+        tree = ET.parse(str(file))
+        self.root = tree.getroot()
+
+    def export(self, output):
+        ET.register_namespace("", "http://www.dgci.gov.pt/2009/Modelo3IRSv2024")
+        tree = ET.ElementTree(self.root)
+        ET.indent(tree, "")
+
+        tree.write(
+            output,
+            encoding="utf-8",
+            xml_declaration=True,
+            short_empty_elements=True,
+        )
--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 import logging
 import typing as t
 import xml.etree.ElementTree as ET
-
+import pathlib
 from degiro import Portfolio
 from irs import IRS
+import argparse
 
 logging.basicConfig(level=logging.DEBUG)
 
 _logger = logging.getLogger(__name__)
+
 
 def create_xml(data, output_file):
     root = ET.Element("root")
@@ -19,12 +21,41 @@ def create_xml(data, output_file):
     tree.write(output_file)
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="A simple argument parser example")
+
+    # Add arguments
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=pathlib.Path,
+        required=True,
+        help="Path to the pre-filled irs declaration xml file",
+    )
+    parser.add_argument(
+        "-d",
+        "--data",
+        type=pathlib.Path,
+        required=True,
+        help="Transaction data from brokers",
+    )
+    parser.add_argument(
+        "-o", "--output", type=pathlib.Path, default="output/output.xml"
+    )
+    # Parse the arguments
+    args = parser.parse_args()
+    return args
+
+
 def main():
-    xml_output_file = "output/output.xml"  # replace with your xml output file path
-    portfolio = Portfolio.from_transaction_csv_files(input_dir="data/276280628")
+    args = parse_arguments()
+    portfolio = Portfolio.from_transaction_csv_files(input_dir=args.data)
     sales, _ = portfolio.declare()
     irs = IRS()
+    irs.load(args.input)
     irs.declare(sales, fiscal_year=2023)
+    irs.export(args.output)
+
 
 if __name__ == "__main__":
     main()
